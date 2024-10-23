022931486e8aa44f45fcb68bcfd6db3df22aacbf
Huy Viet <nguyentranhuyviet04022004@gmail.com>
2024-10-23 20:35:46 +0700

    add create_roles.sql

---
+++ create_roles.sql
@@ -0,0 +1,24 @@
+--DB_customer--
+CREATE ROLE DB_customer;
+GRANT CONNECT ON DATABASE e_bookstore TO DB_customer;
+GRANT USAGE ON SCHEMA app_e_bookstore TO DB_customer;
+GRANT SELECT ON TABLE app_e_bookstore.book, app_e_bookstore.orders, app_e_bookstore.order_item TO DB_customer;
+--DB_author--
+CREATE ROLE DB_author;
+GRANT CONNECT ON DATABASE e_bookstore TO DB_author;
+GRANT USAGE ON SCHEMA app_e_bookstore TO DB_author;
+GRANT SELECT ON TABLE app_e_bookstore.book TO DB_author;
+--DB_Publisher--
+CREATE ROLE DB_Publisher;
+GRANT CONNECT ON DATABASE e_bookstore TO DB_Publisher;
+GRANT USAGE ON SCHEMA app_e_bookstore TO DB_Publisher;
+GRANT SELECT ON TABLE app_e_bookstore.book, app_e_bookstore.author TO DB_Publisher;
+--DB_admin--
+CREATE ROLE DB_admin;
+GRANT CONNECT ON DATABASE e_bookstore TO DB_admin;
+GRANT USAGE ON SCHEMA app_e_bookstore TO DB_admin;
+GRANT SELECT, UPDATE ON ALL TABLES IN SCHEMA app_e_bookstore TO DB_admin; 
+--DB_technical
+CREATE ROLE DB_technical;
+GRANT CONNECT ON DATABASE e_bookstore TO DB_technical;
+GRANT ALL PRIVILEGES ON DATABASE e_bookstore TO DB_technical;
